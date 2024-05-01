b48f824623b396229c9060825ed07f6a1c2f31f4
NONAMEGG <38563106+NONAMEGG@users.noreply.github.com>
2024-05-01 21:03:24 +0300

    Add files via upload

---
+++ dz3/Complex.h
@@ -0,0 +1,79 @@
+#pragma once
+#include <cmath>
+#define M_PI      3,141592653589793
+
+class Complex
+{
+public:
+	double Re, Im;
+	Complex(double Re = 0, double Im = 0);
+
+    double getReal() { return Re; }
+    double getImag() { return Im; }
+
+	Complex operator +(const Complex& c)const
+	{
+		return Complex(Re + c.Re, Im + c.Im);
+	}
+    Complex operator -(const Complex& c) const {
+        return Complex(Re - c.Re, Im - c.Im);
+    }
+
+    Complex operator *(const Complex& c) const {
+        double newRe = Re * c.Re - Im * c.Im;
+        double newIm = Re * c.Im + Im * c.Re;
+        return Complex(newRe, newIm);
+    }
+
+    Complex operator /(const Complex& c) const {
+        if (c.Re == 0 && c.Im == 0) {
+            throw "Division by zero is not allowed";
+        }
+
+        double newRe = (Re * c.Re + Im * c.Im) / (c.Re * c.Re + c.Im * c.Im);
+        double newIm = (Im * c.Re - Re * c.Im) / (c.Re * c.Re + c.Im * c.Im);
+
+        return Complex(newRe, newIm);
+    }
+
+    Complex Conjugate() const {
+        return Complex(Re, -Im);
+    }
+
+    double Mod() const {
+        return std::sqrt(Re * Re + Im * Im);
+    }
+
+    double Arg() const {
+        # define M_PI           3.14159265358979323846
+        if (Mod() == 0) {
+            return 0;
+        }
+        else if (Re > 0) {
+            return std::atan(Im / Re);
+        }
+        else if (Re < 0) {
+            if (Im >= 0) {
+                return M_PI + std::atan(Im / Re);
+            }
+            else {
+                return -M_PI + std::atan(Im / Re);
+            }
+        }
+        else {
+            if (Im > 0) {
+                return M_PI / 2;
+            }
+            else {
+                return -M_PI / 2;
+            }
+        }
+    }
+};
+
+Complex::Complex(double re, double im) 
+{
+	Re = re;
+	Im = im;
+}
+
---
+++ dz3/classes.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include "complex.h"
+using namespace std;
+
+inline ostream& operator << (ostream& o, const Complex& c)
+{
+	return o << '(' << c.Re << ", " << c.Im << ')';
+}
+
+int main()
+{
+	Complex a(1, 2), b = 3;
+	cout << a << ", " << b << endl;
+	cout << a + b << " a + b" << endl;
+
+	Complex v[4] = { 1, 2, Complex(2, 3) };
+
+	for (int i = 0; i < 4; ++i) {
+		cout << v[i];
+	}
+	cout << endl;
+
+    Complex* pc;
+
+    pc = new Complex(1);
+    cout << "Deystvitelinaya chast': " << pc->getReal() << ", Mnimaya chast': " << pc->getImag() << endl;
+    delete pc;
+    pc = new Complex;
+    cout << "                        " << pc->getReal() << ",                 " << pc->getImag() << endl;
+
+    delete pc;
+    pc = new Complex(1, 2);
+    cout << "                        " << pc->getReal() << ",                 " << pc->getImag() << endl;
+    cout << "DEy. chast' cherez -> : " << pc->getReal() << endl;
+    cout << "Mnimaya cherez -> : " << pc->getImag() << endl;
+    delete pc;
+    Complex* pcArray = new Complex[3];
+    cout << pc[1].getReal() << endl;
+    delete[] pcArray;
+}
+
+
